237d82289e4e0b3b1b3b88f4d4a03b5e08269b6d
lexington1988 <lexington88@hotmail.co.uk>
2025-05-20 22:15:35 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,24 @@
-const CACHE_NAME = 'ppc-gas-rate-v1';
-const urlsToCache = [
+const CACHE_NAME = 'ppc-gas-rate-cache-v1';
+const FILES_TO_CACHE = [
   '/',
   '/index.html',
   '/style.css',
   '/script.js',
-  '/icon.png',
+  '/manifest.json',
   '/beep.mp3',
   '/end-beep.mp3',
-  'https://cdn.jsdelivr.net/npm/fuse.js@6.6.2' // external library
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
